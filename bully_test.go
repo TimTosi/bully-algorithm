8513663e5669c3c394a3600f9a09fd5d9f9ab1f4
Tim Tosi <timothee.tosi@gmail.com>
2019-02-06 14:18:50 +0000

    test(bully): add Listen tests

--- bully_test.go
+++ bully_test.go
@@ -68,3 +68,38 @@ func TestBully_NewBully(t *testing.T) {
 		})
 	}
 }
+
+func TestBully_Listen(t *testing.T) {
+	testCases := []struct {
+		name               string
+		mockProto          string
+		mockAddr           string
+		expectedAssertFunc func(assert.TestingT, interface{}, ...interface{}) bool
+	}{
+		{
+			"regular",
+			"tcp4",
+			"127.0.0.1:8100",
+			assert.Nil,
+		},
+		{
+			"badProto",
+			"tcp22",
+			"127.0.0.1:8101",
+			assert.NotNil,
+		},
+		{
+			"badAddr",
+			"tcp6",
+			"mockBadAddr:8102",
+			assert.NotNil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			b := mockBully("1", "127.0.0.1", "1")
+			tc.expectedAssertFunc(t, b.Listen(tc.mockProto, tc.mockAddr))
+		})
+	}
+}
