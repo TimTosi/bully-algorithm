e732bd45cf1f004e87d65e3209911dc8c5f22539
Tim Tosi <timothee.tosi@gmail.com>
2019-02-06 13:56:27 +0000

    doc(bully): update documentation by adding package name

--- peer.go
+++ peer.go
@@ -5,14 +5,14 @@ import (
 	"io"
 )
 
-// Peer is a `struct` representing a remote `Bully`.
+// Peer is a `struct` representing a remote `bully.Bully`.
 type Peer struct {
 	ID   string
 	addr string
 	sock *gob.Encoder
 }
 
-// NewPeer returns a new `Peer`.
+// NewPeer returns a new `*bully.Peer`.
 func NewPeer(ID, addr string, fd io.Writer) *Peer {
 	return &Peer{ID: ID, addr: addr, sock: gob.NewEncoder(fd)}
 }
